9d4ffb8e4b74a75dba364910f6db8b64ce232028
Ana Catarina Basílio <82106354+Ana-Catarina-Basilio@users.noreply.github.com>
2023-11-27 16:16:14 +0000

    Correcting error
    Correcting an error I did in the previous commit.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className= "app-container">
     <h1 className = "introduction">Team Introduction</h1>
       <Team name="Kemi"  hobby="travelling ✈️" reason="I love experiencing new culture" color ="purple" />
-      <Team name ="Catarina" hobby="Kickboxing 🥊" reason="Do you know a better way to let some steam off? It makes me feel empowered!" color="Orange"/>
+      <Team name ="Catarina" hobby="Kickboxing 🥊" reason="there is no better way to let some steam off! It makes me feel empowered!" color="Orange"/>
       <Team name ="" hobby ="" reason ="" color="" />
       <Team name ="" hobby ="" reason ="" color=""/>
       <Team name ="" hobby ="" reason ="" color=""/>
