8a6637f9c226e30533bdb340f657e5aa7b3759d3
laurenjg <laurengray0@hotmail.com>
2023-11-28 15:49:47 +0000

    Lauren update app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Team name="Kemi"  hobby="travelling ✈️" reason="I love experiencing new culture" color ="purple" />
       <Team name ="Catarina" hobby="Kickboxing 🥊" reason="there is no better way to let some steam off! It makes me feel empowered!" color="Orange"/>
       <Team name ="Charlotte" hobby ="Hiking ⛰️" reason ="I love to recharge in nature, having a picnic at a mountain peak is the best!" color="green" />
-      <Team name ="Lauren" hobby ="Karaoke 🎤" reason ="it's an amazing stress-reliever! Plus you get to unleash your inner pop-star ;)" color="red"/>
+      <Team name ="Lauren" hobby ="Karaoke 🎤" reason ="it's an amazing stress-reliever! Plus you get to unleash your inner pop-star! ;)" color="red"/>
       <Team name ="" hobby ="" reason ="" color=""/>
       <Team name ="" hobby ="" reason ="" color=""/>
     </div>
