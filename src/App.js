2131a6826f41f976552e21e6903784a22925d0b3
Upson <jess.upson@hotmail.co.uk>
2023-11-29 13:56:43 +0000

    Adding my details to Team intro

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Team name ="Catarina" hobby="Kickboxing 🥊" reason="there is no better way to let some steam off! It makes me feel empowered!" color="Orange"/>
       <Team name ="Charlotte" hobby ="Hiking ⛰️" reason ="I love to recharge in nature, having a picnic at a mountain peak is the best!" color="green" />
       <Team name ="Lauren" hobby ="Karaoke 🎤" reason ="it's an amazing stress-reliever! Plus you get to unleash your inner pop-star! ;)" color="red"/>
-      <Team name ="" hobby ="" reason ="" color=""/>
+      <Team name ="Jess" hobby ="Mountaineering ⛰️" reason ="I love the challenge and literally pushing myself to new heights!" color="green"/>
       <Team name ="" hobby ="" reason ="" color=""/>
     </div>
   );
