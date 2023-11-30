659c4d5d679ed176b612b45299bf64278bb76550
Kemi Oluwasanmi <130588372+Kemi-ol@users.noreply.github.com>
2023-11-30 18:11:54 +0100

    Update App.js
    added new paragraph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className= "app-container">
     <h1 className = "introduction">Team Introduction</h1>
+    <p> Get to know the team </p>
       <Team name="Kemi"  hobby="travelling ✈️" reason="I love experiencing new culture" color ="purple" />
       <Team name ="Catarina" hobby="Kickboxing 🥊" reason="there is no better way to let some steam off! It makes me feel empowered!" color="Orange"/>
       <Team name ="Charlotte" hobby ="Hiking ⛰️" reason ="I love to recharge in nature, having a picnic at a mountain peak is the best!" color="green" />
