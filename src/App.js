ff7ec664fa9c8825ccbc10e685ceec6498a25923
charlotteswebdev <charlottevmcknight@gmail.com>
2023-12-11 19:37:44 +0000

    removed event files, updated app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+// App.js
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import CategoryBox from './checkboxes/CategoryBox.js';
 import BackButton from './back_button/back_button.js';
 import Map from './map/map.js';
-import wonderMapCategories from './events/wonderMapCatergories';
-import getWonderMapEvents from './events/wonderMapEvents';
 import WelcomePage from './screens/welcomePage'; 
-
 
 function App() {
   const [showWelcome, setShowWelcome] = useState(true);
   const [categories, setCategories] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
-  const [selectedEvents, setSelectedEvents] = useState([]);
 
+  // Fetch categories from the api url at the first render
   useEffect(() => {
     fetch('http://localhost:3000/api/categories')
       .then(response => response.json())
@@ -21,6 +19,7 @@ function App() {
       .catch(error => console.error('Error fetching categories:', error));
   }, []);
 
+  // handle change of checkbox using filter and spread operator
   const handleCheckboxChange = (category, isChecked) => {
     if (isChecked) {
       setSelectedCategories((prevCategories) => [...prevCategories, category]);
@@ -30,22 +29,6 @@ function App() {
       );
     }
   };
-
-  const handleEventCheckboxChange = (event, isChecked) => {
-    if (isChecked) {
-      setSelectedEvents((prevEvents) => [...prevEvents, event]);
-    } else {
-      setSelectedEvents((prevEvents) =>
-        prevEvents.filter((selectedEvent) => selectedEvent !== event)
-      );
-    }
-  };
-
-  const handleSubmit = () => {
-    console.log('Selected Categories:', selectedCategories);
-    console.log('Selected Events:', selectedEvents);
-  };
-
   const handleExploreClick = () => {
     setShowWelcome(false); // Hide the welcome page when Explore is clicked
   };
@@ -63,34 +46,27 @@ function App() {
       {showWelcome ? (
         <WelcomePage onExploreClick={handleExploreClick} />
       ) : (
-        <div>
+        <>
           <div className="checkboxesDiv">
-            {categories.map((categoryData) => (
-              <CategoryBox
-                key={categoryData.id}
-                id={categoryData.id.toLowerCase().replace(/\s+/g, '')}
-                checkboxCategory={categoryData.label}
-                onChange={(isChecked) => handleCheckboxChange(categoryData.label, isChecked)}
-              />
-            ))}
-            {wonderMapCategories.map((category) => (
-              <CategoryBox
-                key={category.toLowerCase().replace(/\s+/g, '')}
-                id={category.toLowerCase().replace(/\s+/g, '')}
-                checkboxCategory={category}
-                onChange={(isChecked) => handleCheckboxChange(category, isChecked)}
-              />
-            ))}
+            {categories.map((categoryData) => {
+              console.log('categoryData', categoryData); // Check the categoryData in the console
+              return (
+                <CategoryBox
+                  key={categoryData.id}
+                  id={categoryData.id.toLowerCase().replace(/\s+/g, '')}
+                  checkboxCategory={categoryData.label}
+                  onChange={(isChecked) => handleCheckboxChange(categoryData.label, isChecked)}
+                />
+              );
+            })}
           </div>
-
+          <div className="mapDiv">
+            <Map selectedCategories={selectedCategories} />
+          </div>
           <div className="footer">
-            <button onClick={getWonderMapEvents}>Submit</button>
-            <div className="mapDiv">
-              <Map selectedEvents={selectedEvents} />
-            </div>
-            <p>4 Code Girls!</p>
+            {/* Footer content */}
           </div>
-        </div>
+        </>
       )}
     </div>
   );
