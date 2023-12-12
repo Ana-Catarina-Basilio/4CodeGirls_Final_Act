9b1d816f4f42d7207b93b6d48fba9b5d466fc197
Ana Catarina Basílio <82106354+Ana-Catarina-Basilio@users.noreply.github.com>
2023-12-12 16:39:59 +0000

    Creating collect_user_information, create_booking, get_user_id and create_booking
    Creation of several functions to fetch information from the Database.
    
    collect_user_information - will allow user to input their own data (name, surname and email). I also added code to check if the email is correct/valid.
    
    we can than choose between create_booking or get_user_id + create_booking. The first one compile the user ID and creates a booking that prints out a confirmation with the users name, the event name and the booking ID.

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -73,10 +73,128 @@ def get_all_categories():
 
         cur.close()
 
-    except:
-        print('Something went wrong')
+    except Exception as exc:
+        raise DBConnectionError('Failed to connect to database') from exc
 
     finally:
         if db_connection:
             db_connection.close()
-            print('DB connection closed')
+            print('DB connection closed')    
+
+
+# This function will allow us to ask for the user information, making sure it introduces a valid email. 
+           
+def collect_user_information():
+
+    def is_valid_email(email):
+        # email validation option 
+        email_pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
+        return bool(re.match(email_pattern, email))
+
+    try:
+        while True:
+            user_first_name = input("Enter your first name: ")
+            user_surname = input("Enter your surname: ")
+            user_email = input("Enter your email: ")
+
+            if is_valid_email(user_email):
+                return user_first_name, user_surname, user_email
+            else:
+                print("Invalid email format. Please enter a valid email.")
+
+    except Exception as exc:
+        print(f"An error occurred: {exc}")
+
+
+# Ww can use this OR
+
+def create_booking(user_info, event_id):
+    try:
+        db_connection = connect_to_database(db_credentials[0], db_credentials[1], db_credentials[2], db_credentials[3])
+        cur = db_connection.cursor()
+        print('Connected to DB')
+
+        user_first_name, user_surname, user_email = user_info
+        query_user_id = 'SELECT UserID FROM Users WHERE User_FirstName = %s AND User_Surname = %s AND User_Email = %s'
+        cur.execute(query_user_id, (user_first_name, user_surname, user_email))
+        result_user = cur.fetchone()
+
+        query_event_name = 'SELECT name FROM Events WHERE EventID = %s'
+        cur.execute(query_event_name, (event_id,))
+        result_event = cur.fetchone()
+
+
+        if result_user and result_event:
+            user_id = result_user[0]
+            event_name = result_event[0]
+
+            query_booking = 'INSERT INTO Bookings (UserID, EventID) VALUES (%s, %s)'
+            cur.execute(query_booking, (user_id, event_id))
+            db_connection.commit()
+
+            booking_id = cur.lastrowid
+
+            print(f"Booking created for {user_info} for event '{event_name}'")
+        else:
+            print("User or event not found. Unable to create booking.")
+
+    except Exception as exc:
+        db_connection.rollback()
+        raise DBConnectionError('Failed to create booking') from exc
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+#or this version separated into more functions
+
+def get_user_id(user_info):
+    try:
+        db_connection = connect_to_database(db_credentials[0], db_credentials[1], db_credentials[2], db_credentials[3])
+        cur = db_connection.cursor()
+
+        user_first_name, user_surname, user_email = user_info
+        query_user_id = 'SELECT UserID FROM Users WHERE User_FirstName = %s AND User_Surname = %s AND User_Email = %s'
+        cur.execute(query_user_id, (user_first_name, user_surname, user_email))
+        result_user = cur.fetchone()
+
+        return result_user[0] if result_user else None
+
+    except Exception as exc:
+        raise DBConnectionError('Failed to fetch user details') from exc
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+def create_booking(user_id, event_id):
+    try:
+        db_connection = connect_to_database(db_credentials[0], db_credentials[1], db_credentials[2], db_credentials[3])
+        cur = db_connection.cursor()
+
+        query_event_name = 'SELECT name FROM Events WHERE EventID = %s'
+        cur.execute(query_event_name, (event_id,))
+        result_event = cur.fetchone()
+
+        if result_event:
+            event_name = result_event[0]
+
+            query_booking = 'INSERT INTO Bookings (UserID, EventID) VALUES (%s, %s)'
+            cur.execute(query_booking, (user_id, event_id))
+            db_connection.commit()
+
+            booking_id = cur.lastrowid
+
+            print(f"Booking created for User ID {user_id} for event '{event_name}' (Booking ID: {booking_id})")
+        else:
+            print("Event not found. Unable to create booking.")
+
+    except Exception as exc:
+        db_connection.rollback()
+        raise DBConnectionError('Failed to create booking') from exc
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
