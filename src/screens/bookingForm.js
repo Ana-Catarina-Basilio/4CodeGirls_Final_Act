d022a23f41de7796f4a5053cf16b01453450d8ee
SophiaAthena <147653452+SophiaAthena@users.noreply.github.com>
2023-12-13 13:02:55 -0500

    Update bookingForm.js to collect user information and allow reservations.

--- src/screens/bookingForm.js
+++ src/screens/bookingForm.js
@@ -1,18 +1,41 @@
 // BookingForm.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './bookingForm.css';
 
 const BookingForm = () => {
   const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
 
   const handleReserve = () => {
+    // Perform reservation logic with userName, userEmail, etc.
     navigate('/bookingConfirmation');
   };
 
   return (
     <div className="form">
-      <h2>You have selected: </h2>
+      <h1>You have selected:</h1>
+      {/* Display the selected item or details here */}
+
+      <div className="detailsSection">
+        <label htmlFor="userName">User Name:</label>
+        <input
+          type="text"
+          id="userName"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <label htmlFor="userEmail">Email:</label>
+        <input
+          type="email"
+          id="userEmail"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+      </div>
+
       <button className="reserveButton" type="button" onClick={handleReserve}>
         Reserve
       </button>
