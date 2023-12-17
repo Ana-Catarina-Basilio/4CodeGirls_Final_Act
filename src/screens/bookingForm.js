f21193b030984218c082705cf0b5b22178df18e5
Kemi Oluwasanmi <130588372+Kemi-ol@users.noreply.github.com>
2023-12-17 23:10:56 +0000

    Update bookingForm.js

--- src/screens/bookingForm.js
+++ src/screens/bookingForm.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './bookingForm.css';
 import { useSelector, useDispatch } from 'react-redux';
